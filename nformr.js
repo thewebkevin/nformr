66004c1c27a31584c1fea9fdce8dd59273d62f56
unknown <kdupuy@WEBPROD-03-PC.wbrz.com>
2015-09-15 17:01:26 -0500

    Removed syntax errors in Javascript. Moved multiple variables to global.

--- nformr.js
+++ nformr.js
@@ -1,27 +1,42 @@
-$(function () {
-    $("button").click(function () {
+/*global $, jQuery*/
+var peopleKilled;
+var peopleInjured;
+var city;
+var cityAPI;
+var state;
+var crime;
+var streetAddress;
+var addressAPI;
+var lawLong;
+var lawShort;
+var day;
+var injured = " ";
+var killed = " ";
+var tfKilled;
+var tfInjured;
+var killedTense;
+var injuredTense;
+var addAnd;
+var keyAPI = "AIzaSyANnAoaacT1T4QC7Kd9W6e9muA2CZb9FIM";
+var googleMapUrl;
+var generated = "<p>";
 
-        var peopleKilled = document.getElementById('peopleKilled').value;
-        var peopleInjured = document.getElementById('peopleInjured').value;
-        var city = document.getElementById('city').value;
-        var cityAPI = city.replace(" ", "+");
-        var state = document.getElementById('state').value;
-        var crime = document.getElementById('crime').value.toLowerCase();
-        var streetAddress = document.getElementById('streetAddress').value;
-        var addressAPI = streetAddress.replace(" ", "+");
-        var lawLong = document.getElementById('lawLong').value;
-        var lawShort;
-        var day = document.getElementById('day').value;
-        var injured = " ";
-        var killed = " ";
-        var tfKilled;
-        var tfInjured;
-        var killedTense;
-        var injuredTense;
-        var addAnd;
-        var keyAPI = "AIzaSyANnAoaacT1T4QC7Kd9W6e9muA2CZb9FIM";
-        var googleMapUrl = "https://maps.googleapis.com/maps/api/staticmap?size=400x400&markers=" + streetAddress + "+" + city + "+"
-        state + "&key=" + keyAPI;
+$(function () {
+    "use strict";
+    $("#generateButton").click(function () {
+
+        peopleKilled = document.getElementById('peopleKilled').value;
+        peopleInjured = document.getElementById('peopleInjured').value;
+        city = document.getElementById('city').value;
+        cityAPI = city.replace(" ", "+");
+        state = document.getElementById('state').value;
+        crime = document.getElementById('crime').value.toLowerCase();
+        streetAddress = document.getElementById('streetAddress').value;
+        addressAPI = streetAddress.replace(" ", "+");
+        lawLong = document.getElementById('lawLong').value;
+        day = document.getElementById('day').value;
+        googleMapUrl = "https://maps.googleapis.com/maps/api/staticmap?size=400x400&markers=" + streetAddress + "+" + city + "+" +
+            state + "&key=" + keyAPI;
 
         if (peopleKilled > 0) {
             tfKilled = true;
@@ -67,7 +82,7 @@ $(function () {
         var generated = "<p>";
         generated += city.toUpperCase() + " - ";
         generated += lawShort + " say " + killed + addAnd + injured + " in a " + crime + " in " + city + " " + day + ".</p>";
-        generated += "<p>According to the " + lawLong + ", " + killed + addAnd + injured + " in a " + crime + " at " + streetAddress + " in " + city + " " + day + ". </p>"
+        generated += "<p>According to the " + lawLong + ", " + killed + addAnd + injured + " in a " + crime + " at " + streetAddress + " in " + city + " " + day + ". </p>";
         generated += "<p>This is a developing story. Stay tuned to <a href ='http://www.wbrz.com/'>WBRZ News 2</a> on <a href ='https://www.facebook.com/WBRZNews2'>Facebook</a> and <a href ='https://twitter.com/wbrz'>Twitter</a> for the lastest updates as they become available.</p>";
 
         if ($('#mapCheck').is(':checked')) {
